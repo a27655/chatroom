49ce483348872c40a5fc2068ffb95e197c699e6e
PC\Y&G <tf27655@gmail.com>
2016-10-24 10:18:33 +0800

    第一次提交

---
+++ models.py
@@ -0,0 +1,121 @@
+import hashlib
+import os
+from flask import session
+from flask_sqlalchemy import SQLAlchemy
+import time
+from flask import Flask
+
+
+
+app = Flask(__name__)
+app.secret_key = 'qweqadasdafasrwqaerxcvzv'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# 指定数据库的路径
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
+
+db = SQLAlchemy(app)
+
+
+class ModelMixin(object):
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        properties = ('{0} = {1}'.format(k, v) for k, v in self.__dict__.items())
+        return '<{0}: \n  {1}\n>'.format(class_name, '\n  '.join(properties))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def time(self):
+        dt = int(time.time())
+        return  dt
+
+
+class User(db.Model, ModelMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    avatar = db.Column(db.String())
+    created_time = db.Column(db.Integer, default=0)
+    captcha = db.Column(db.String())
+    # 下面定义关系
+    messages = db.relationship('Message', backref='user', foreign_keys='Message.user_id')
+
+
+    def __init__(self, form):
+        super(User, self).__init__()
+        self.username = form.get('username', '')
+        self.password = form.get('password', '')
+        self.created_time = self.time()
+        self.avatar = form.get('avatar', '/static/img/1.jpg')
+        self.captcha = form.get('captcha', '')
+    # 验证注册用户的合法性的
+    def valid(self):
+        u = User.query.filter_by(username=self.username).first()
+        valid_username = User.query.filter_by(username=self.username).first() == None
+        valid_username_len = len(self.username) >= 4
+        valid_password_len = len(self.password) >= 4
+        valid_captcha = self.captcha == '吃瓜群众'
+        msgs = ['注册成功，请登录']
+        if not valid_username:
+            message = '用户名已经存在'
+            msgs.append(message)
+        elif not valid_username_len:
+            message = '用户名长度必须大于等于 4'
+            msgs.append(message)
+        elif not valid_password_len:
+            message = '密码长度必须大于等于 4'
+            msgs.append(message)
+        elif not valid_captcha:
+            message = '验证码必须输入 ‘吃瓜群众’'
+            msgs.append(message)
+        status = valid_username and valid_username_len and valid_password_len and valid_captcha
+        return status, msgs
+
+    # 验证用户登录
+    def validate_login(self, form):
+        username = form.get('username')
+        password = form.get('password')
+        username_equals = username == self.username
+        password_equals = password == self.password
+        if username_equals and password_equals:
+            status = True
+            msg = '登录成功'
+        else:
+            status = False
+            msg = '用户名或密码错误'
+        return status, msg
+
+
+class Message(db.Model, ModelMixin):
+    __tablename__ = 'Messages'
+    # 下面是字段定义
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String())
+    created_time = db.Column(db.String(), default=0)
+    # 外键
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, form):
+
+        self.content = form.get('content', '')
+        self.created_time = self.time()
+        self.channel = form.get('channel', '')
+
+
+def db_build():
+    db.drop_all()
+    db.create_all()
+    print('rebuild database')
+
+
+if __name__ == '__main__':
+    # 先 drop_all 删除所有数据库中的表
+    # 再 create_all 创建所有的表
+    db_build()
---
+++ redischat.py
@@ -0,0 +1,149 @@
+import flask
+from flask import request
+import redis
+import time
+import json
+
+
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from flask import abort
+
+from user import *
+
+def current_user():
+    uid = session.get('user_id')
+    if uid is not None:
+        u = User.query.get(uid)
+        return u
+    else:
+        return None
+
+def admin_required(f):
+    @wraps(f)
+    def function(*args, **kwargs):
+        u = current_user()
+        if u == None:
+            return render_template('index.html', user=u)
+
+        return f(*args, **kwargs)
+
+    return function
+
+
+@main.route('/chatroom/login', methods=['POST'])
+def login():
+    form = request.form
+    username = form.get('username','')
+    user = User.query.filter_by(username=username).first()
+    status, msg = user.validate_login(form)
+    if status:
+        session['user_id'] = user.id
+        return render_template('index.html', message=msg, user=user)
+    else:
+        user = current_user()
+        return render_template('index.html', message=msg, user=user)
+#
+@main.route('/chatroom/register', methods=['POST'])
+def register():
+    form = request.form
+    uu = current_user()
+    u = User(form)
+    status, msgs = u.valid()
+    if status:
+        u.save()
+        message = msgs[0]
+        return render_template('index.html', message=message, user=uu)
+    else:
+        message = msgs[1]
+        return render_template('index.html', message=message, user=uu)
+
+'''
+# 使用 gunicorn 启动
+gunicorn --worker-class=gevent -t 9999 redischat:app
+# 开启 debug 输出
+gunicorn --log-level debug --worker-class=gevent -t 999 redis_chat81:app
+# 把 gunicorn 输出写入到 gunicorn.log 文件中
+gunicorn --log-level debug --access-logfile gunicorn.log --worker-class=gevent -t 999 redis_chat81:app
+'''
+
+# 连接上本机的 redis 服务器
+# 所以要先打开 redis 服务器
+red = redis.Redis(host='localhost', port=6379, db=0)
+print('redis', red)
+
+app = flask.Flask(__name__)
+app.secret_key = 'pfsdfhloi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+# 发布聊天广播的 redis 频道
+chat_channel = 'chat'
+
+
+
+def stream():
+    '''
+    监听 redis 广播并 sse 到客户端
+    '''
+    # 对每一个用户 创建一个[发布订阅]对象
+    pubsub = red.pubsub()
+    # 订阅广播频道
+    pubsub.subscribe(chat_channel)
+    # 监听订阅的广播
+    for message in pubsub.listen():
+        print(message)
+        if message['type'] == 'message':
+            data = message['data'].decode('utf-8')
+            # 用 sse 返回给前端
+            yield 'data: {}\n\n'.format(data)
+
+
+@app.route('/chatroom/subscribe')
+@admin_required
+def subscribe():
+    return flask.Response(stream(), 
+        mimetype="text/event-stream")
+
+
+@app.route('/chatroom/')
+def index_view():
+    u = current_user()
+    return flask.render_template('index.html', user=u)
+
+
+def current_time():
+    return int(time.time())
+
+
+@app.route('/chatroom/chat/add', methods=['POST'])
+@admin_required
+def chat_add():
+    msg = request.get_json()
+    user = msg.get('user', '')
+    content = msg.get('content', '')
+    channel = msg.get('channel', '')
+    r = {
+        'user': user,
+        'content': content,
+        'channel': channel,
+        'created_time': current_time(),
+    }
+    message = json.dumps(r, ensure_ascii=False)
+    print('debug', message)
+    # 用 redis 发布消息
+    m = Message(r)
+    m.save()
+    red.publish(chat_channel, message)
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    config = dict(
+        host='0.0.0.0',
+        debug=True,
+    )
+    app.run(**config)
